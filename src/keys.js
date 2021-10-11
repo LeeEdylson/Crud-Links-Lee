db08f65114a04abd3f46d5395e11bf0e551c6686
LeeEdylson <66333627+LeeEdylson@users.noreply.github.com>
2021-10-10 21:42:49 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3443545',
+        password: '4svtCpEnUw',
+        database: 'sql3443545'
+    }
+
+};
